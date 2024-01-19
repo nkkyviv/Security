f5e162ea515abd44fa139b72991183e3fdaee631
vivian <nkkyvivian@yahoo.com>
2024-01-19 21:20:19 +0100

    made some changes to my protocol

--- server/lib/protocol/protocol.h
+++ server/lib/protocol/protocol.h
@@ -9,7 +9,6 @@ void protocol_init(uint32_t speed);
 
 size_t protocol_send(uint8_t *buffer, size_t size);
 
-//size_t protocol_receive(uint8_t *buffer, size_t size);
 size_t protocol_receive(char *buffer, size_t size);
 
 #endif
